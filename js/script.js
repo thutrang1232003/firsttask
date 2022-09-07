2c87630c103119e3cc236d3210c83e4ec078edc7
Trang Tran <77712536+thutrang1232003@users.noreply.github.com>
2022-09-07 23:22:01 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// const arrowDowns = document.querySelectorAll('.services i');
+// for (const arrowDown of arrowDowns) {
+//     arrowDown.onclick = myfunction();
+
+//     function myfunction() {
+//         document.getElementsByClassName(".content").classList.toggle("active");
+//     }
+// }
+
